e588f241ef7c3b9e8053c239257984be617aee03
Florencia Rocca <florenciarocca@live.com>
2022-06-22 10:51:27 -0300

    Error en  ==

--- MaquinaEstado/Ej3.hpp
+++ MaquinaEstado/Ej3.hpp
@@ -36,7 +36,7 @@ class S_Machine {
         {
         case ACUMULAR:
             adder  += sgn[count];
-            if(count == N)
+            if(count == (N-1))
                 {
                     count = 0;
                     state = PROMEDIAR;
